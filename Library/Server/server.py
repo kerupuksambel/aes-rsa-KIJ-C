14786f0818f636e4cda58f3db55e36dd8282eb3b
patrick-cw <patrick1528@gmail.com>
2021-10-12 09:30:27 +0700

    Update server.py

--- Library/Server/server.py
+++ Library/Server/server.py
@@ -30,12 +30,12 @@ with open(filename, "wb") as f:
         #read bytes from socket
         file_in = open(filename, "rb")
         private_key = RSA.import_key(open("private.pem").read())
-        enc_session_key, nonce, tag, ciphertext = \
+        enc_sessionkey, nonce, tag, ciphertext = \
             [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
 
         # Decrypt the session key with the private RSA key
         cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
+        session_key = cipher_rsa.decrypt(enc_sessionkey)
 
         # Decrypt the data with the AES session key
         cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
