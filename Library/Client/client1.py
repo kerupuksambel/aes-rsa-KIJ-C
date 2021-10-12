2fd7d6d263673284ebd27ee5768a13a0f3c0ea41
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2021-10-12 11:18:47 +0700

    Debugging done for lib impl.

--- Library/Client/client1.py
+++ Library/Client/client1.py
@@ -1,21 +1,21 @@
 from socket import *
 import os, base64
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
+
+from time import sleep
 
 AES_KEY_SIZE = 16
 BLOCK_SIZE = 16
 
+sessionkey = get_random_bytes(AES_KEY_SIZE)
 
-def encrypt(filename):
-    rsa_key = RSA.import_key(open("receiver.pem").read())
+def encrypt(data):
+    rsa_key = RSA.importKey(open("receiver.pem").read())
     cipher_rsa = PKCS1_OAEP.new(rsa_key)
     
-    with open(filename,'rb') as f:
-        data = f.read()
     #Generate a session key randomly
-    sessionkey = get_random_bytes(AES_KEY_SIZE)
 
     #Encrypt session key with RSA public key
     enc_sessionkey = cipher_rsa.encrypt(sessionkey)
@@ -28,18 +28,16 @@ def encrypt(filename):
     file_out = open("encrypted_data.bin", "wb")
     [ file_out.write(x) for x in (enc_sessionkey, cipher_aes.nonce, tag, ciphertext) ]
     file_out.close()
-        
-    return file_out
+
+    # Reread
+    res = open("encrypted_data.bin", 'rb').read()
+
+    return res
 
 
-def send_file(filename,host,port):
-    #connect to socket
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    client_address = (host, port)
-    client_socket.connect(client_address)
-
+def send_file(filename, sock):
     #send filename 
-    client_socket.send(f"{filename}".encode())
+    sock.send(f"{filename}".encode())
     
     #send file process
     with open(f"{filename}", "rb") as f:
@@ -48,17 +46,58 @@ def send_file(filename,host,port):
             data = f.read(4096)
             print(data)
             #send all bytes
-            client_socket.sendall(encrypt(data))
+            sock.sendall(encrypt(data))
 
     # close the socket
-    client_socket.close()
+    sock.close()
+
+def send_rsa(msg, sock):
+    rsa_key = RSA.importKey(open("receiver.pem").read())
+    cipher_rsa = PKCS1_OAEP.new(rsa_key)
+    enc_msg = cipher_rsa.encrypt(msg)
+
+    print("Encrypted : " + __import__('base64').b64encode(enc_msg).decode())
+
+    sock.send(enc_msg)
+    sleep(1)
+
+def send_aes(msg, sock):
+    cipher_aes = AES.new(sessionkey, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(msg)
+
+    # Send those values for decryption later
+    send_rsa(cipher_aes.nonce, sock)
+    send_rsa(tag, sock)
+
+    sock.send(ciphertext)
+
+
 
 if __name__ == "__main__":
-    host = input('Masukkan IP address yang ingin dituju : ')
-    port = int(input('Masukkan port yang ingin dituju : '))
-    filename = input('Masukkan Nama File yang ingin dikirim :')
-    # encrypt_file = encrypt(filename)
-    send_file(filename,host,port)
+    argv = __import__('sys').argv
+    if(len(argv)) == 1:
+        host = input('Masukkan IP address yang ingin dituju : ')
+        port = int(input('Masukkan port yang ingin dituju : '))
+        filename = input('Masukkan Nama File yang ingin dikirim :')
+    else:
+        host = 'localhost'
+        port = 9001
+        filename = 'test.txt'
+    file_content = open(filename, 'rb').read()
+    
+    # connect to socket
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_address = (host, port)
+    client_socket.connect(client_address)
+    print("Session Key : " + __import__('base64').b64encode(sessionkey).decode())
+    # send key 
+    send_rsa(sessionkey, client_socket)
+    # send name
+    send_rsa(filename.encode(), client_socket)
+    # send file
+    send_aes(file_content, client_socket)
+
+    client_socket.close()
 
 
 
--- Library/Server/server.py
+++ Library/Server/server.py
@@ -1,9 +1,12 @@
 from socket import *
 import os
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import AES, PKCS1_OAEP
 
-server_address = ('0.0.0.0',9000)
+from base64 import b64encode
+from time import time
+
+server_address = ('0.0.0.0',9001)
 
 # create the server socket
 # TCP socket
@@ -18,34 +21,40 @@ server_socket.listen(1)
 # accept connection
 client_socket, client_address = server_socket.accept() 
 
-# if below code is executed, that means the sender is connected
 print(f"[+] {client_address} is connected.")
 
-# receive the file 
-filename = client_socket.recv(4096).decode()
+# Load RSA priv key 
+def recv_rsa(sock):
+	private_key = RSA.importKey(open("private.pem").read())
+	cipher_rsa = PKCS1_OAEP.new(private_key)
+	enc_msg = sock.recv(4096)
+	print('New encrypted message : ' + b64encode(enc_msg).decode())
 
-# start receive file
-with open(filename, "wb") as f:
-    while True:
-        #read bytes from socket
-        file_in = open(filename, "rb")
-        private_key = RSA.import_key(open("private.pem").read())
-        enc_sessionkey, nonce, tag, ciphertext = \
-            [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+	decrypted_msg = cipher_rsa.decrypt(enc_msg)
 
-        # Decrypt the session key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_sessionkey)
+	return decrypted_msg
 
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        decrypted_data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-        data = decrypted_data
+def recv_aes(sock, key, nonce, tag):
+	cipher_aes = AES.new(key, AES.MODE_EAX, nonce)
+	ciphertext = sock.recv(4096)
+	data = cipher_aes.decrypt_and_verify(ciphertext, tag)
 
-        # write to the file the bytes we just received
-        f.write(data)
+	return data
 
-print(f"{filename} Received!")
+key = recv_rsa(client_socket)
+print("base64(key) : " + b64encode(key).decode())
+name = recv_rsa(client_socket).decode()
+print("Name : " + name)
+nonce = recv_rsa(client_socket)
+print("base64(nonce) : " + b64encode(nonce).decode())
+tag = recv_rsa(client_socket)
+print("base64(tag) : " + b64encode(tag).decode())
+
+file = recv_aes(client_socket, key, nonce, tag)
+print(f"{name} Received!")
+
+with open('saved/' + name + '_' + str(int(time())), 'wb') as f:
+	f.write(file)
        
 
 # close the client socket
